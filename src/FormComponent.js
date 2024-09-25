62439dace5611fd1a6d440c5b57778d382c5f88b
Kavya Sri Kasarla <kkasarla@iu.edu>
2024-09-25 11:36:41 -0400

    New update

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -7,7 +7,7 @@ const FormComponent = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await axios.post('http://localhost:5000/api/form', data);
+      const response = await axios.post('https://testkasarla.netlify.app/api/form', data);
       if (response.status === 200) {
         alert('Form submitted successfully!');
       }
